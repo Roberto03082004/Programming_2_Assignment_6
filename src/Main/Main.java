45ee43ee9c31d573384fcd232efd80e3cadc97f0
Roberto03082004 <70285112+Roberto03082004@users.noreply.github.com>
2023-12-07 16:16:51 -0500

    Initial Commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,2 +1,53 @@
-package Main;public class Main {
+package Main;
+
+import util.HugeInt;
+
+public class Main {
+    public static void main(String[] args) {
+        // Testing code...
+
+        // 1. setValue
+        HugeInt h1 = new HugeInt();
+        h1.setValue(1234);
+        System.out.println("1) h1: " + h1); // Output should be 1234
+
+        // 2. getValue
+        int value = h1.getValue();
+        System.out.println("2) Value of h1: " + value); // Output should be 1234
+
+        // 3. clone
+        HugeInt h1Clone = h1.clone();
+        System.out.println("3) h1Clone: " + h1Clone); // Output should be 1234
+
+        // 4. log
+        long digitsCount = h1.log();
+        System.out.println("4) Digits count in h1: " + digitsCount); // Output should be 4
+
+        // 5. modExp
+        HugeInt h1ModExp = h1.modExp(2);
+        System.out.println("5) h1 modExp(2): " + h1ModExp); // Output should be 34 (1234 % 100)
+
+        // 6. quotientExp
+        HugeInt h1QuotientExp = h1.quotientExp(2);
+        System.out.println("6) h1 quotientExp(2): " + h1QuotientExp); // Output should be 12 (1234 / 100)
+
+        // 7. timesExp
+        HugeInt h1TimesExp = h1.timesExp(2);
+        System.out.println("7) h1 timesExp(2): " + h1TimesExp); // Output should be 123400
+
+        // 8. add
+        HugeInt h2 = new HugeInt(5678);
+        HugeInt sum = h1.add(h2);
+        System.out.println("8) h1 + h2: " + sum); // Output should be 6912 (1234 + 5678)
+
+        // 9. modNine
+        HugeInt h3 = new HugeInt(1011111 - 1);
+        HugeInt h3ModNine = h3.modNine();
+        System.out.println("9) h3 modNine: " + h3ModNine); // Output should be 0
+
+        // 10. product
+        HugeInt product = h1.product(h2);
+        System.out.println("10) h1 * h2: " + product); // Output should be 7006652 (1234 * 5678)
+    }
 }
+
--- src/util/HugeInt.java
+++ src/util/HugeInt.java
@@ -1,2 +1,172 @@
-package util;public class HugeInt {
+package util;
+
+import java.util.LinkedList;
+
+public class HugeInt {
+    private LinkedList<Byte> digits;
+
+    // Default constructor initializes a HugeInt with zero
+    public HugeInt() {
+        digits = new LinkedList<>();
+        digits.add((byte) 0);
+    }
+
+    // Constructor initializes a HugeInt with an integer value
+    public HugeInt(int value) {
+        digits = new LinkedList<>();
+        setValue(value);
+    }
+
+    // Method sets the value of the HugeInt with an integer
+    public void setValue(int value) {
+        digits.clear(); // Clear existing digits
+        String strValue = Integer.toString(value);
+        for (char c : strValue.toCharArray()) {
+            digits.addFirst((byte) (c - '0'));
+        }
+    }
+
+    // Method returns the integer value of the HugeInt
+    public int getValue() {
+        if (digits.size() == 1 && digits.getFirst() == 0) {
+            // If the number is zero or has leading zeros, return 0
+            return 0;
+        }
+
+        // Calculate the integer value
+        int result = 0;
+        for (byte digit : digits) {
+            result = result * 10 + digit;
+        }
+
+        return result;
+    }
+
+
+    // Method returns a string representation of the HugeInt
+    @Override
+    public String toString() {
+        return digits.toString().replaceAll("[^0-9]", "");
+    }
+
+    // Method returns a copy of the HugeInt
+    public HugeInt clone() {
+        HugeInt clone = new HugeInt();
+        clone.digits = new LinkedList<>(this.digits);
+        return clone;
+    }
+
+    // Method returns the number of digits in the HugeInt
+    public long log() {
+        return digits.size();
+    }
+
+    // Method calculates the remainder when dividing the HugeInt by 10^n
+    public HugeInt modExp(long n) {
+        HugeInt result = new HugeInt();
+        int newSize = (int) Math.min(n, digits.size());
+        result.digits = new LinkedList<>(digits.subList(0, newSize));
+        return result;
+    }
+
+    // Method calculates the quotient when dividing the HugeInt by 10^n
+    public HugeInt quotientExp(long n) {
+        HugeInt result = new HugeInt();
+        int newSize = (int) Math.max(0, digits.size() - n);
+        result.digits = new LinkedList<>(digits.subList(newSize, digits.size()));
+        return result;
+    }
+
+    // Method calculates the product of 10^n and the HugeInt
+    public HugeInt timesExp(long n) {
+        HugeInt result = new HugeInt();
+        result.digits.addAll(digits);
+        for (long i = 0; i < n; i++) {
+            result.digits.addLast((byte) 0);
+        }
+        return result;
+    }
+
+    // Method adds another HugeInt to the current one
+    public HugeInt add(HugeInt h) {
+        HugeInt result = new HugeInt();
+        LinkedList<Byte> resultDigits = result.digits;
+        LinkedList<Byte> hDigits = h.digits;
+
+        int carry = 0;
+        int maxSize = Math.max(digits.size(), hDigits.size());
+
+        for (int i = 0; i < maxSize || carry != 0; i++) {
+            int x = i < digits.size() ? digits.get(i) : 0;
+            int y = i < hDigits.size() ? hDigits.get(i) : 0;
+            int sum = x + y + carry;
+            resultDigits.add((byte) (sum % 10));
+            carry = sum / 10;
+        }
+
+        // Remove leading zeros
+        while (resultDigits.size() > 1 && resultDigits.getLast() == 0) {
+            resultDigits.removeLast();
+        }
+
+        return result;
+    }
+
+    // Method calculates the remainder when dividing the HugeInt by 9
+    public HugeInt modNine() {
+        int sum = 0;
+        for (byte digit : digits) {
+            sum += digit;
+        }
+
+        if (sum < 10) {
+            return new HugeInt(sum);
+        } else {
+            return new HugeInt(sum).modNine();
+        }
+    }
+
+    // Method calculates the product of the HugeInt and another HugeInt
+    public HugeInt product(HugeInt h) {
+        HugeInt result = new HugeInt();
+        LinkedList<Byte> resultDigits = result.digits;
+
+        for (int i = 0; i < digits.size(); i++) {
+            for (int j = 0; j < h.digits.size(); j++) {
+                int x = digits.get(i);
+                int y = h.digits.get(j);
+                int product = x * y;
+                int position = i + j;
+
+                // Ensure there's enough space in the result
+                while (resultDigits.size() <= position) {
+                    resultDigits.addLast((byte) 0);
+                }
+
+                int sum = resultDigits.get(position) + product;
+                resultDigits.set(position, (byte) (sum % 10));
+
+                // Propagate carry
+                int carry = sum / 10;
+                position++;
+                while (carry > 0) {
+                    while (resultDigits.size() <= position) {
+                        resultDigits.addLast((byte) 0);
+                    }
+
+                    sum = resultDigits.get(position) + carry;
+                    resultDigits.set(position, (byte) (sum % 10));
+                    carry = sum / 10;
+                    position++;
+                }
+            }
+        }
+
+        // Remove leading zeros
+        while (resultDigits.size() > 1 && resultDigits.getLast() == 0) {
+            resultDigits.removeLast();
+        }
+
+        return result;
+    }
 }
